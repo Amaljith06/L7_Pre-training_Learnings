0d703dc4c79b2acc37bf105ab60a7a42a062d8c1
Amaljith Kumar <amaljithkumar006@gmail.com>
2025-06-19 14:36:00 +0530

    update assignment 3

--- Java/Assignment 3 - Stdnt_Mg_Sys/src/com/litmus7/sms/StudentApp.java
+++ Java/Assignment 3 - Stdnt_Mg_Sys/src/com/litmus7/sms/StudentApp.java
@@ -4,21 +4,30 @@ import java.util.Scanner;
 
 import com.litmus7.sms.dto.Student;
 
+/**
+ * This class represents the main application to manage and display student
+ * records
+ */
 public class StudentApp {
+	/**
+	 * The main method
+	 * 
+	 * @param args
+	 */
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the number of students: ");
 		int n;
 		n = scan.nextInt();
-		Student s[] = new Student[n];
-		for(int i=0; i<n; i++) {
-			System.out.println("Enter details of student " + (i+1));
-			s[i] = new Student();
-			s[i].inputDetails();
+		Student students[] = new Student[n];
+		for (int i = 0; i < n; i++) {
+			System.out.println("Enter details of student " + (i + 1));
+			students[i] = new Student();
+			students[i].inputDetails();
 		}
-		for(int i=0; i<n; i++) {
-			s[i].printReportCard();
+		for (int i = 0; i < n; i++) {
+			students[i].printReportCard();
 		}
-		
+
 	}
 }
--- Java/Assignment 3 - Stdnt_Mg_Sys/src/com/litmus7/sms/dto/Student.java
+++ Java/Assignment 3 - Stdnt_Mg_Sys/src/com/litmus7/sms/dto/Student.java
@@ -2,55 +2,96 @@ package com.litmus7.sms.dto;
 
 import java.util.Scanner;
 
+/**
+ * This class represents a student with name, roll number and marks in 5
+ * subjects
+ * 
+ */
 public class Student {
 	private String name;
 	private int rollNo;
 	private double marks[] = new double[5];
 
+	/**
+	 * Enum for student grades
+	 */
+	private enum Grade {
+		A, B, C, D, F
+	}
+
+	/**
+	 * This method takes input from user for students name, roll number and subject
+	 * marks
+	 */
 	public void inputDetails() {
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.print("Enter name: ");
 		name = scan.nextLine();
 		System.out.print("Enter roll number: ");
 		rollNo = scan.nextInt();
 		System.out.println("Enter marks for 5 subjects");
-		for(int i=0; i<5; i++) {
-			System.out.print("Subject" + (i+1) + ": ");
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Subject" + (i + 1) + ": ");
 			marks[i] = scan.nextDouble();
 		}
 	}
+
+	/**
+	 * This method calculates the total marks obtained by the student
+	 * 
+	 * @return total marks
+	 */
 	public double calculateTotal() {
 		double total = 0;
-		
-		for(double mark : marks) {
+
+		for (double mark : marks) {
 			total += mark;
 		}
 		return total;
 	}
+
+	/**
+	 * This method calculates the average marks of the student
+	 * 
+	 * @return average marks
+	 */
 	private double calculateAverage() {
 		double avg;
-		
+
 		avg = calculateTotal() / 5;
 		return avg;
 	}
-	private String getGrades() {
+
+	/**
+	 * This method determines the grade of the student based on average marks
+	 * 
+	 * @param <Grade>
+	 * 
+	 * @return grade as a string
+	 */
+	private Grade getGrades() {
 		double avg = calculateAverage();
-		
-		if(avg >= 90) { 
-			return "A";
-		} else if(avg >= 75) {
-			return "B";
-		} else if(avg >= 60) {
-			return "C";
-		} else if(avg >= 50) {
-			return "D";
+
+		if (avg >= 90) {
+			return Grade.A;
+		} else if (avg >= 75) {
+			return Grade.B;
+		} else if (avg >= 60) {
+			return Grade.C;
+		} else if (avg >= 50) {
+			return Grade.D;
 		} else {
-			return "F";
+			return Grade.F;
 		}
 	}
+
+	/**
+	 * This method prints the report card of the student
+	 */
 	public void printReportCard() {
 		System.out.println("\n---Report Card---\n");
-		System.out.printf("Name: %s%nRoll Number: %d%nTotal Marks: %.2f%nAverage Marks: %.2f%nGrade: %s%n",name, rollNo, calculateTotal(), calculateAverage(), getGrades());
+		System.out.printf("Name: %s%nRoll Number: %d%nTotal Marks: %.2f%nAverage Marks: %.2f%nGrade: %s%n", name,
+				rollNo, calculateTotal(), calculateAverage(), getGrades());
 	}
 }
